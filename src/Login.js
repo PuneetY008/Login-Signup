5bd58cfa0e0b0f138b0fda292db8145fcad223e0
Puneet Yadav <puneetyadav380@gmail.com>
2022-02-20 19:49:20 +0530

    done

---
+++ src/Login.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class Login extends Component{
+    render(){
+        return(
+            <div className='Login'>
+                <h2>Login Budd!!</h2>
+                <form className='Login-form'>
+                    <div className='login-form-input'>
+                        <label for="username">Username</label>
+                        <input id='username' type="text" className='form-inp' placeholder='username@gmail.com' />
+                    </div>
+                    <div className='login-form-input'>
+                        <label for="password">Password</label>
+                        <input id='password' type="text" className='form-inp' placeholder='********' />
+                    </div>
+                    <div className='user-settings'>
+                        <div className='remember-box'>
+                            <input type="checkbox" id='remember-checkbox'/>
+                            <label for="remember-checkbox">Remember Me</label>
+                        </div>
+                        <a href='#'>Forgot Password?</a>
+                    </div>
+                    <button type='submit' className='form-btn'>LogIn</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
