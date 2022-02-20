8a163b9d84635aec1fc67f7f743824b51c6c33b3
Puneet Yadav <puneetyadav380@gmail.com>
2022-02-20 21:06:06 +0530

    header addition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './Login';
 import Signup from './Signup';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Login />
     </div>
   );
